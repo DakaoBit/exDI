4c55aba327ba0839c6939e8ac2a67d632a13a719
DakaoBit <oasis.wang1016@gmail.com>
2019-10-17 16:04:27 +0800

    DakaoBit 2019/10/17 - Ch02： Decorator 模式

---
+++ Ch02/Decorator/DecoratedLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch02.NullObject;
+
+namespace Ch02.Decorator
+{
+    class DecoratedLogger : ILogger
+    {
+        private ILogger logger;
+
+        public DecoratedLogger(ILogger log)
+        {
+            logger = log;
+        }
+
+        public void Log(string msg)
+        {
+            logger.Log(DateTime.Now.ToString() + " - " + msg);
+        }
+    }
+}
---
+++ Ch02/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ch02.Decorator;
+using Ch02.NullObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NullObject Pattern
+            //ILogger log = new ConsoleLogger();
+            //var demo = new Ch02.NullObject.MainApp();
+            //demo.DoSomething(log);
+
+            //Decorator Pattern
+            ILogger logger = new DecoratedLogger(new ConsoleLogger());
+            logger.Log("Hello, 裝飾模式!");
+        }
+
+    }
+}
